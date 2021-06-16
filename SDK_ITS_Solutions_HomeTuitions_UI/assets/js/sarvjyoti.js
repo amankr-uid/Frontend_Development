ef3d5dde53042eb91ef04d6e93f6e46bb74b15f5
AMAN KUMAR <amankr.lang@gmail.com>
2021-06-16 11:15:53 +0530

    SDK ITS SOLUTIONS HOME TUITIONS WEBSITE
    This website is design for gathering student to join this firm for giving home tuitions service for school students & for foreign students.
    
    This website is designed & developing with using HTML5, CSS3, JavaScript, JQuery, Bootstrap, PHP, Photoshop.

---
+++ SDK_ITS_Solutions_HomeTuitions_UI/assets/js/sarvjyoti.js
@@ -0,0 +1,48 @@
+
+/*=================
+pop up
+==================*/
+
+
+  $(document).ready(function(){
+    $("#myModal").modal('show');
+  });
+
+  setTimeout(function() {
+    $('#myModal').modal('hide');
+}, 15000);
+
+/*=================
+End 
+==================*/
+
+
+/*=================
+Twak Chat
+==================*/
+
+
+/*var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5bb2e4e2b033e9743d01f158/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();*/
+
+/*=================
+
+Twak Chat
+==================*/
+
+/*=================
+
+
+==================*/
+
+/*=================
+
+Twak Chat
+==================*/
